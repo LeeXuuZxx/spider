c71e08044a4cc4b9e527ad0d3cfac9764f582da8
LeeXuuZxx <65284418+LeeXuuZxx@users.noreply.github.com>
2024-07-10 12:13:14 +0800

    spider

--- leexuu/quotesbot/spiders/spider.py
+++ leexuu/quotesbot/spiders/spider.py
@@ -10,6 +10,10 @@ import csv
 import time
 import aiohttp
 import asyncio
+from enum import Enum
+
+class TopicEnum(Enum):
+    ANNOUNCEMENT = 131203  # 公告
 
 class Spider(scrapy.Spider):
     name = "Spider"
@@ -43,8 +47,11 @@ class Spider(scrapy.Spider):
         'HTTPERROR_ALLOWED_CODES': [403],
     }
     
-    TELEGRAM_BOT_TOKEN = '7283290474:AAHnBvxcxlYqQFBa4r2RjykTee8H6eQAgSQ'
-    TELEGRAM_CHAT_ID = '2115436972'
+    # TELEGRAM_BOT_TOKEN = '7283290474:AAHnBvxcxlYqQFBa4r2RjykTee8H6eQAgSQ'
+    # TELEGRAM_CHAT_ID = '2115436972'
+    TELEGRAM_BOT_TOKEN = '6752350542:AAEaBk2n3rImEWEhCBMwIbBU1Wq01zkN_54'
+    TELEGRAM_CHAT_ID = '-1001958551048'
+    
 
     # 用于存储公告URL的字典，避免重复
     article_urls = {'binance': {}, 'binance_api': {}, 'okx': {}, 'okx_announcements-api': {}, 'okx_api': {}, 'gate': {}, 'gate_api': {}, 'bitget': {}, 'bybit': {}, 'bybit_delist': {}, 'bybit_api': {}}
@@ -73,9 +80,6 @@ class Spider(scrapy.Spider):
         logger.info(f"已爬取 {self.count_times} 次")
         logger.info(f"当前时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}")
 
-            
-
-
     def bitget(self, url):
         params = {'language': 'zh_CN'}
         try:
@@ -101,8 +105,6 @@ class Spider(scrapy.Spider):
         except requests.exceptions.RequestException as e:
             print(f"Request error: {e}")
 
-
-
     def check_loop_start(self):
         if self.is_finished():  # 当队列中没有请求时休眠并重启
             self.logger.info('本轮采集完毕，%s秒后开始下一轮采集' % self.sleep_time)
@@ -110,7 +112,6 @@ class Spider(scrapy.Spider):
             return self.start_requests()
         return []
     
-
     def is_finished(self):  # 检查队列中是否还有请求
         if self.crawler.engine.downloader.active:
             return False
@@ -119,8 +120,6 @@ class Spider(scrapy.Spider):
         if self.crawler.engine.slot.scheduler.has_pending_requests():
             return False
         return True
-
-
 
     def parse(self, response):
         exchange = response.meta['exchange']
@@ -230,155 +229,157 @@ class Spider(scrapy.Spider):
         if exchange == 'okx_api':
             app_data_selector = 'div.content'
             app_data_text = response.css(app_data_selector).extract()
-            # logger.info(app_data_text)
             if not app_data_text:
                 logger.error(f"No data found for {exchange}")
                 return
-            
-            # 将列表中的HTML内容合并为一个字符串
             app_data_text = ''.join(app_data_text)
-            try:
-                # 使用BeautifulSoup解析HTML
-                soup = BeautifulSoup(app_data_text, 'html.parser')
+            soup = BeautifulSoup(app_data_text, 'html.parser')
+            base_url = 'https://www.okx.com'
+            
+            h1_tags = soup.find_all('h1')
+            for h1_tag in h1_tags:
+                current_date = h1_tag.get_text(strip=True)
+                ul_tag = h1_tag.find_next('ul')
+                if ul_tag:
+                    for li_tag in ul_tag.find_all('li'):
+                        title_content = li_tag.get_text(strip=True)
+                        a_tag = li_tag.find('a', href=True)
+                        if a_tag:
+                            title = f"{current_date} {title_content}"
+                            url = base_url + a_tag['href']
+                            if title not in self.article_urls['okx_api']:
+                                self.article_urls['okx_api'][title] = url
+                                time.sleep(1)
 
-                # 初始化变量来存储当前日期和标题信息
-                current_date = None
-                current_title = None
-
-                # 找到所有的<h1>标签和<ul>标签
-                for h1_tag in soup.find_all('h1'):
-                    if h1_tag.has_attr('id'):
-                        current_date = h1_tag['id']  # 获取日期
-                        current_title = h1_tag.text.strip()  # 获取标题
-
-                    # 查找当前日期下的所有新增接口链接
-                    ul_tag = h1_tag.find_next('ul')
-                    if ul_tag:
-                        for li_tag in ul_tag.find_all('li'):
-                            a_tag = li_tag.find('a')
-                            if a_tag:
-                                title = current_title + ' ' + li_tag.text.strip()  # 构建完整标题
-                                url = a_tag['href']  # 获取链接URL
-                                if title not in self.article_urls['okx_api']:
-                                    self.article_urls['okx_api'][title] = url  # 存储到字典中
-                # logger.info(f"当前交易所{exchange}爬取公告数量：{len(self.article_urls[exchange])}")
-
-                # 打印或保存 article_urls 字典
-                # logger.info(f"OKX: Total unique articles: {len(self.article_urls['okx_api'])}")
-                # logger.info(f"OKX: Article URLs: {self.article_urls['okx_api']}")
-
-            except Exception as e:
-                logger.error(f"Error parsing HTML: {e}")
+            # logger.info(f"当前交易所{exchange}爬取公告数量：{len(self.article_urls[exchange])}")
 
         if exchange == 'okx':
-            app_data_selector = 'li.index_article__15dX1'
+            app_data_selector = 'div.home-container'
             app_data_text = response.css(app_data_selector).extract()
             if not app_data_text:
                 logger.error(f"No data found for {exchange}")
                 return
-            
-            for article_html in app_data_text:
-                article_url = response.urljoin(article_html.split('href="')[1].split('"')[0])
-                article_title = article_html.split('index_title__6wUnB">')[1].split('</div>')[0]
-                
-                if article_title not in self.article_urls['okx']:
-                    self.article_urls['okx'][article_title] = article_url
-                    # logger.info(f"OKX: New article URL: {article_url}")
-                # else:
-                #     logger.info(f"OKX: Duplicate article found: {article_title}, skipping")
-            # logger.info(f"当前交易所{exchange}爬取公告数量：{len(self.article_urls[exchange])}")
+            app_data_text = ''.join(app_data_text)
+            soup = BeautifulSoup(app_data_text, 'html.parser')
+            base_url = 'https://www.okx.com'
+            announcements = soup.select('a.index_articleItem__d-8iK') #这里是公告对应的index_articleItem，可能会更换
+            for announcement in announcements:
+                title = announcement.select_one('div.index_articleTitle__ys7G7').get_text(strip=True) #这里是公告对应的index_articleTitle，可能会更换
+                url = base_url + announcement['href']
+                # logger.info(f"Title: {title}, URL: {url}")
 
-            # logger.info(f"OKX: Total unique articles: {len(self.article_urls['okx'])}")
-            # logger.info(f"OKX: Article URLs: {self.article_urls['okx']}")
+                if title not in self.article_urls[exchange]:
+                    self.article_urls[exchange][title] = url
+
+            # logger.info(f"当前交易所{exchange}爬取公告数量：{len(self.article_urls[exchange])}")
                     
         if exchange == 'okx_announcements-api':
-            app_data_selector = 'li.index_article__15dX1'
+            app_data_selector = 'div.home-container'
             app_data_text = response.css(app_data_selector).extract()
             if not app_data_text:
                 logger.error(f"No data found for {exchange}")
                 return
+            app_data_text = ''.join(app_data_text)
+            soup = BeautifulSoup(app_data_text, 'html.parser')
+            base_url = 'https://www.okx.com'
             
-            for article_html in app_data_text:
-                article_url = response.urljoin(article_html.split('href="')[1].split('"')[0])
-                article_title = article_html.split('index_title__6wUnB">')[1].split('</div>')[0]
-                
-                if article_title not in self.article_urls['okx_announcements-api']:
-                    self.article_urls['okx_announcements-api'][article_title] = article_url
-                    # logger.info(f"OKX: New article URL: {article_url}")
-                # else:
-                #     logger.info(f"OKX: Duplicate article found: {article_title}, skipping")
-            # logger.info(f"当前交易所{exchange}爬取公告数量：{len(self.article_urls[exchange])}")
+            announcements = soup.select('a.index_articleItem__d-8iK')
+            for announcement in announcements:
+                title_tag = announcement.select_one('div.index_articleTitle__ys7G7')
+                date_tags = announcement.select('span[data-testid="DateDisplay"]')
+                if title_tag and len(date_tags) >= 2:
+                    title = title_tag.get_text(strip=True)
+                    published_date = date_tags[0].get_text(strip=True)
+                    updated_date = date_tags[1].get_text(strip=True) if len(date_tags) > 1 else ""
+                    full_title = f"{title}发布于 {published_date}更新于 {updated_date}"
+                    url = base_url + announcement['href']
+                    # logger.info(f"Title: {full_title}, URL: {url}")
+                    if full_title not in self.article_urls['okx_announcements-api']:
+                        self.article_urls['okx_announcements-api'][full_title] = url
+                        time.sleep(1)
 
-            # logger.info(f"OKX: Total unique articles: {len(self.article_urls['okx_announcements-api'])}")
-            # logger.info(f"OKX: Article URLs: {self.article_urls['okx_announcements-api']}")
+            # logger.info(f"当前交易所{exchange}爬取公告数量：{len(self.article_urls[exchange])}")
 
         if exchange == 'bybit_delist':
-            app_data_selector = 'div.article-item'
-            app_data_text = response.css(app_data_selector)
+            app_data_selector = 'div.app'
+            app_data_text = response.css(app_data_selector).extract()
             if not app_data_text:
-                logger.error("No data found")
+                logger.error(f"No data found for {exchange}")
                 return
-                        
-            for idx, app_data in enumerate(app_data_text):
-                article_title = app_data.css('.article-item-title span::text').get()
-                article_url = response.urljoin(app_data.css(f'{app_data_selector}[data-cy="announcement-{idx}"]::attr(href)').get())
-                if article_title and article_url:
-                    if article_title not in self.article_urls['bybit_delist']:
-                        self.article_urls['bybit_delist'][article_title] = article_url
-                        # self.logger.info(f"Bybit: New article URL: {article_url}")
-                    # else:
-                    #     self.logger.info(f"Bybit: Duplicate article found: {article_title}, skipping")
-                else:
-                    logger.error(f"Missing title or URL in article: {app_data}")
-            # logger.info(f"当前交易所{exchange}爬取公告数量：{len(self.article_urls[exchange])}")
+            app_data_text = ''.join(app_data_text)
+            soup = BeautifulSoup(app_data_text, 'html.parser')
+            base_url = 'https://announcements.bybit.com'
+            announcements = soup.select('a.no-style')
+            for announcement in announcements:
+                title_tag = announcement.select_one('div.article-item-title span')
+                date_tag = announcement.select_one('div.article-item-date')
+                if title_tag and date_tag:
+                    title = title_tag.get_text(strip=True)
+                    published_date = date_tag.get_text(strip=True)
+                    full_title = f"{title}发布于 {published_date}"
+                    relative_url = announcement['href']
+                    url = f"{base_url}{relative_url}"
+                    # logger.info(f"Title: {full_title}, URL: {url}")
+                    if full_title not in self.article_urls['bybit_delist']:
+                        self.article_urls['bybit_delist'][full_title] = url
+                        time.sleep(1)
+            # logger.info(f"当前交易所 {exchange} 爬取公告数量：{len(announcements)}")
            
         if exchange == 'bybit':
-            app_data_selector = 'div.article-item'
-            app_data_text = response.css(app_data_selector)
+            app_data_selector = 'div.app'
+            app_data_text = response.css(app_data_selector).extract()
             if not app_data_text:
-                logger.error("No data found")
+                logger.error(f"No data found for {exchange}")
                 return
-            for idx, app_data in enumerate(app_data_text):
-                article_title = app_data.css('.article-item-title span::text').get()
-                article_url = response.urljoin(app_data.css(f'{app_data_selector}[data-cy="announcement-{idx}"]::attr(href)').get())
-                if article_title and article_url:
-                    if article_title not in self.article_urls['bybit']:
-                        self.article_urls['bybit'][article_title] = article_url
-                        # self.logger.info(f"Bybit: New article URL: {article_url}")
-                    # else:
-                    #     self.logger.info(f"Bybit: Duplicate article found: {article_title}, skipping")
-                else:
-                    logger.error(f"Missing title or URL in article: {app_data}")
-            # logger.info(f"当前交易所{exchange}爬取公告数量：{len(self.article_urls[exchange])}")
-
+            app_data_text = ''.join(app_data_text)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            base_url = 'https://announcements.bybit.com'
+            announcements = soup.select('a.no-style')
+            for announcement in announcements:
+                title_tag = announcement.select_one('div.article-item-title span')
+                date_tag = announcement.select_one('div.article-item-date')
+                if title_tag and date_tag:
+                    title = title_tag.get_text(strip=True)
+                    published_date = date_tag.get_text(strip=True)
+                    full_title = f"{title}发布于 {published_date}"
+                    relative_url = announcement['href']
+                    url = f"{base_url}{relative_url}"
+                    # logger.info(f"Title: {full_title}, URL: {url}")
+                    if full_title not in self.article_urls['bybit']:
+                        self.article_urls['bybit'][full_title] = url
+                        time.sleep(1)
+            # logger.info(f"当前交易所 {exchange} 爬取公告数量：{len(announcements)}")
+           
         if exchange == 'gate':
-            articles = response.css('div.article-list-item')
-            if not articles:
-                logger.error(f"No articles found for {exchange}")
+            app_data_selector = 'div.article-list-box'
+            app_data_text = response.css(app_data_selector).extract()
+            if not app_data_text:
+                logger.error(f"No data found for {exchange}")
                 return
-            for article in articles:
-                try:
-                    article_url = response.urljoin(article.css('a::attr(href)').get())
-                    article_title = article.css('h3 span::text').get()
-                    if article_title and article_url:
-                        if article_title not in self.article_urls['gate']:
-                            self.article_urls['gate'][article_title] = article_url
-                            # logger.info(f"Gate: New article URL: {article_url}")
-                        # else:
-                        #     logger.info(f"Gate: Duplicate article found: {article_title}, skipping")
-                    else:
-                        logger.error(f"Missing title or URL in article: {article}")
-                except Exception as e:
-                    logger.error(f"Error parsing article: {article}. Error: {e}")
-            # logger.info(f"当前交易所{exchange}爬取公告数量：{len(self.article_urls[exchange])}")
+            app_data_text = ''.join(app_data_text)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            base_url = 'https://www.gate.io'
+            announcements = soup.select('div.article-list-item')
 
+            for announcement in announcements:
+                title_tag = announcement.select_one('a.article-list-item-title h3 span')
+                date_tag = announcement.select_one('span.article-list-info-timer span')
+                if title_tag and date_tag:
+                    title = title_tag.get_text(strip=True)
+                    published_date = date_tag.get_text(strip=True)
+                    full_title = f"{title}发布于 {published_date}"
+                    relative_url = announcement.select_one('a.article-list-item-title')['href']
+                    url = f"{base_url}{relative_url}"
+                    # logger.info(f"Title: {full_title}, URL: {url}")
+                    if full_title not in self.article_urls['gate']:
+                        self.article_urls['gate'][full_title] = url
+                        time.sleep(1)
+            # logger.info(f"当前交易所 {exchange} 爬取公告数量：{len(announcements)}")
 
-            # logger.info(f"Gate: Total unique articles: {len(self.article_urls['gate'])}")
-            # logger.info(f"Gate: Article URLs: {self.article_urls['gate']}")
-
-        if exchange == 'binance':
+        if exchange == 'binance': #权限不够，只能拿__APP_DATA
             app_data_selector = '//*[@id="__APP_DATA"]/text()'
             app_data_text = response.xpath(app_data_selector).get()
+            logger.info(app_data_text)
             if not app_data_text:
                 logger.error(f"No data found for {exchange}")
                 return
@@ -408,8 +409,7 @@ class Spider(scrapy.Spider):
             # logger.info(f"当前交易所{exchange}爬取公告数量：{len(self.article_urls[exchange])}")
         
         asyncio.run(self.save_to_csv())
-
-
+        
         for request in self.check_loop_start():
             yield request
 
@@ -417,12 +417,14 @@ class Spider(scrapy.Spider):
         # 打印或保存 article_urls 字典
         # logger.info(f"{exchange}: Total unique articles: {len(self.article_urls[exchange])}")
         # logger.info(f"{exchange}: Article URLs: {self.article_urls[exchange]}")
-    async def send_telegram_message(self, message):
+    async def send_telegram_message(self, message, topic_enum: TopicEnum):
         url = f"https://api.telegram.org/bot{self.TELEGRAM_BOT_TOKEN}/sendMessage"
+        reply_to_message_id = topic_enum.value
         data = {
             "chat_id": self.TELEGRAM_CHAT_ID,
             "text": message,
-            "parse_mode": "HTML"
+            "parse_mode": "HTML",
+            "reply_to_message_id": reply_to_message_id
         }
         try:
             async with aiohttp.ClientSession() as session:
@@ -457,7 +459,8 @@ class Spider(scrapy.Spider):
                             new_articles_count += 1
                     for article in new_articles:
                         message = f"New announcement from {exchange}:\nTitle: {article['Title']}\nURL: {article['URL']}"
-                        await self.send_telegram_message(message)
+                        await self.send_telegram_message(message, topic_enum=TopicEnum.ANNOUNCEMENT)
+                        time.sleep(0.5)
                         logger.info(message)
         except Exception as e:
             logger.error(f"Error saving to CSV: {e}")
